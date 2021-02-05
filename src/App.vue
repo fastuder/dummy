c756c17d8feddfb20c27048fd7356e6f50b748c2
Studer Lian HSLU <lian.studer@hslu.ch>
2021-02-05 16:53:54 +0100

    API fetch and list rendering

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div v-for="person in persons" :key="person.id">
+      <Person :firstName="person.name"></Person>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import Person from './components/Person.vue';
+import axios from "axios";
+
+// PersonInterface heisst PersonInterface, 
+// da es das Objekt Person schon gibt und IPerson mag ESLint nicht.
+interface PersonInterface {
+  id: number;
+  imageUrl: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  contactNumber: string;
+  age: number;
+  dob: string;
+  salary: number;
+  address: string;
+}
+
+async function getPersons<P>(uri: string): Promise<P> {
+  return await axios.get(uri);
+}
 
 @Component({
   components: {
-    HelloWorld,
+    Person,
   },
 })
-export default class App extends Vue {}
-</script>
+export default class App extends Vue {
+  persons = getPersons<PersonInterface[]>("https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001");
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-</style>
+</script>
